9e721dfccb6efeee38c3fe4661ced766406447a4
Riki Permana <arwahdevops@gmail.com>
2025-05-15 15:07:50 +0700

    add local build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# build.sh
+
+set -e # Keluar jika ada error
+
+VERSION="0.2.0" # Ganti ini untuk versi berikutnya
+COMMIT_HASH=$(git rev-parse --short HEAD)
+BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+BUILT_BY="build-script"
+
+LDFLAGS="-X 'main.version=$VERSION' \
+         -X 'main.commit=$COMMIT_HASH' \
+         -X 'main.date=$BUILD_DATE' \
+         -X 'main.builtBy=$BUILT_BY'"
+
+echo "Building PhotonSR version $VERSION..."
+go build -ldflags="$LDFLAGS" -o photonsr ./cmd
+echo "Build complete: ./photonsr"
+echo "Verifying version:"
+./photonsr -version
